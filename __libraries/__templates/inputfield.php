6fdf00f6b1116982708e8930a250b73fb56def11
Dharshan Kumar <155003114+dharshan-kumarj@users.noreply.github.com>
2024-02-01 12:41:45 +0530

    source.py working prpoerly

---
+++ __libraries/__templates/inputfield.php
@@ -0,0 +1,20 @@
+<div>
+    <div class="container" style="
+    position:absolute;
+    bottom:0;left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;">
+        <div class="row">
+            <div class="input-group mb-3">
+                <input id = "userinput" name ="userinput" type="text" class="form-control" placeholder="Enter Question" >
+                <div class="input-group-append">
+                    <button id="Execute" type="button" class="btn btn-primary" onclick="Ajax()">Send</button>
+                </div>
+            </div>
+    
+        
+        </div>
+    </div>
+</div>
+
