2c20ae2ee60fcdfa77e59d469dc429b315091030
Dharshan Kumar <155003114+dharshan-kumarj@users.noreply.github.com>
2024-02-01 12:42:24 +0530

    Delete __libraries directory

--- __libraries/__templates/output_screen.php
+++
@@ -1,4 +0,0 @@
-<div class="container">
-<pre id="output_screen"></pre>
-
-</div>
--- __libraries/load.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function load_template($template){
-    include $_SERVER['DOCUMENT_ROOT']."/IoTbot/__libraries/__templates/$template";
-  }
-?>
