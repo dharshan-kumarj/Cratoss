db112abf7059fedc6a7229689aeeccd16bf7a8ed
Dharshan Kumar <155003114+dharshan-kumarj@users.noreply.github.com>
2024-02-07 00:12:52 +0530

    solved #3 in this commit and last commit

---
+++ inputfield.php
@@ -0,0 +1,29 @@
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<div>
+    <div class="container" style="
+    position:absolute;
+    bottom:0;left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    bottom:30;
+    ">
+        <div class="row">
+            <div class="input-group mb-3">
+           
+                <input id = "userinput" name ="userinput" type="text" class="form-control" placeholder="Ask me...." >
+             
+                <div class="input-group-append">
+                    <button id="Execute" type="button" class="btn btn-primary" onclick="Ajax() ">Send</button>
+                    
+                </div>
+            </div>
+    
+        
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
