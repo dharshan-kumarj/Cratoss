af402bd5ba94935d86a4ca89d176f1ec43a33e50
dharshan-kumarj <jdharshankumar@gmail.com>
2024-07-21 10:05:46 +0530

    updated Backend with Gemini AI

--- IoTbot/__libraries/__py_/source.py
+++ IoTbot/__libraries/__py_/source.py
@@ -1,40 +1,38 @@
-import argparse     #for executing the command line python programs 
-import json        #to give direct output by removibg the key value pair
-from openai import OpenAI  #to use ai chatbot using API keys
-import os   
-from dotenv import load_dotenv  #to load the api key from .env
+import argparse
+import json
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
 
-
-def Argument():
+def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', '-i',help='Enter input')    #defining the arguparser function getting input from user using argparser
-    args = vars(parser.parse_args())
+    parser.add_argument('--input', '-i', help='Enter input')
+    args = parser.parse_args()
     return args
 
-user_input= Argument()['input'] #gets the input by using argparser
-load_dotenv()  #unload the API keys from .env
+def configure_genai():
+    load_dotenv()
+    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-client = OpenAI(                            #from 17 to 35 it is for to generate ai answer
-    api_key=os.getenv("OPEN_API_KEY")
-)
+def generate_response(prompt):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(prompt)
+    return response.text
 
-def structured_generator(prompt):
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": prompt
-            },
-        ],
-        model="gpt-3.5-turbo"
-    )
-    return chat_completion
+def main():
+    args = parse_arguments()
+    user_input = args.input
 
-prompt = ( f"You are an IoT technician,You need to answer if the question is only Related to Iot and if not,say it is out of scope question {user_input}")
-result=structured_generator(prompt)
-output=result.choices[0].message.content
+    configure_genai()
 
-dict_=dict()
-dict_={"output":output}
-final_out=json.dumps(dict_)    #to remove the key value pair
-print(final_out)
+    prompt = (f"You are an IoT technician. Answer the following question if it's related to IoT. "
+              f"If it's not related to IoT, state that it's out of scope: {user_input}")
+
+    output = generate_response(prompt)
+
+    result = {"output": output}
+    final_out = json.dumps(result)
+    print(final_out)
+
+if __name__ == "__main__":
+    main()
